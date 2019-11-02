efa73baf0f06c859b74ff25f33f1aa013931074b
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-02 23:36:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     description='Data Science essentials',
     author='Roshan T John',
     author_email='roshan.github@gmail.com',
+    url = 'https://github.com/RTJ19/dsutils_dev.git',
+    keywords = ['PYSPARK', 'EDA']
     zip_safe=False,
     install_requires=['pandas'
                       , 'numpy'
@@ -21,5 +23,15 @@ setup(
                       # , 'spark_testing_base' # Only required for testing
                       # , 'findspark' # Only required for testing
                       ]
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" for development or "5 - Production/Stable" 
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 
 )
