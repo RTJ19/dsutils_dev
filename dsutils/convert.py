541b584f8675d26513605e8e7354d97b681c77b6
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-03 12:59:29 +0530

    Create convert.py

---
+++ dsutils/convert.py
@@ -0,0 +1,32 @@
+"""
+Github user zaloogarcia have developed some function that will help in 
+conversion pandas dataframe to pyspark
+https://gist.github.com/zaloogarcia/11508e9ca786c6851513d31fb2e70bfc
+"""
+from pyspark.sql.types import *
+
+# Auxiliar functions
+def equivalent_type(f):
+    if f == 'datetime64[ns]': return DateType()
+    elif f == 'int64': return LongType()
+    elif f == 'int32': return IntegerType()
+    elif f == 'float64': return FloatType()
+    else: return StringType()
+
+def define_structure(string, format_type):
+    try: typo = equivalent_type(format_type)
+    except: typo = StringType()
+    return StructField(string, typo)
+
+# Given pandas dataframe, it will return a spark's dataframe.
+def pandas_to_spark(pandas_df):
+    columns = list(pandas_df.columns)
+    types = list(pandas_df.dtypes)
+    struct_list = []
+    i = 0
+    for column, typo in zip(columns, types): 
+      #print (column,typo)
+      struct_list.append(define_structure(column, typo))
+    p_schema = StructType(struct_list)
+    #print (p_schema)
+    return spark.createDataFrame(pandas_df, p_schema)
