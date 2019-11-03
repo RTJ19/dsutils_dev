608358403dd19e1ff735b2ab5dbce23cc997aa6f
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-03 13:02:30 +0530

    Update __init__.py

--- dsutils/__init__.py
+++ dsutils/__init__.py
@@ -1,4 +1,5 @@
 from dsutils_dev.dsutils.evaluate import get_eda_plots
+from dsutils_dev.dsutils.convert import equivalent_type,define_structure,pandas_to_spark
 __version__ = '0.0.1'
 
 #from dsutils_dev.evaluate import get_eda_plots
