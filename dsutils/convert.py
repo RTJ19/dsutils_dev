93acd1bc08d876a7c3c3dc81a27adc3b1ed1cf94
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-03 13:48:44 +0530

    Update convert.py

--- dsutils/convert.py
+++ dsutils/convert.py
@@ -1,32 +1,85 @@
 """
-Github user zaloogarcia have developed some function that will help in 
+Github user zaloogarcia have developed some functions that will help in 
 conversion pandas dataframe to pyspark
+
 https://gist.github.com/zaloogarcia/11508e9ca786c6851513d31fb2e70bfc
+https://stackoverflow.com/questions/37513355/converting-pandas-dataframe-into-spark-dataframe-error/56895546#56895546
+
+It was further forked by tonyfraser into a class format which creates
+a dict type that contains df.dtypes AND type(pd.columnname)
+
+https://gist.github.com/tonyfraser/79a255aa8a9d765bd5cf8bd13597171e
+
+Looks something like this.
+  {  'stringtypecolumn': {'dtype': 'object', 'actual': 'str'},
+     'act_num': {'dtype': 'int32', 'actual': 'numpy.int32'},
+     'text_dat': {'dtype': 'object', 'actual': 'list'},
+     'scene_description': {'dtype': 'object', 'actual': 'NoneType'},
+     'keywords': {'dtype': 'object', 'actual': 'list'}}
+
 """
-from pyspark.sql.types import *
 
-# Auxiliar functions
-def equivalent_type(f):
-    if f == 'datetime64[ns]': return DateType()
-    elif f == 'int64': return LongType()
-    elif f == 'int32': return IntegerType()
-    elif f == 'float64': return FloatType()
-    else: return StringType()
+from pyspark.shell import sqlContext
+from pyspark.sql.types import StringType, StructField, ArrayType, IntegerType, FloatType, DateType, LongType, StructType
 
-def define_structure(string, format_type):
-    try: typo = equivalent_type(format_type)
-    except: typo = StringType()
-    return StructField(string, typo)
+class DataFrameConverter:
+    """A class that takes in a pandas data frame and converts it to a spark data frame..."""
 
-# Given pandas dataframe, it will return a spark's dataframe.
-def pandas_to_spark(spark_session, pandas_df):
-    columns = list(pandas_df.columns)
-    types = list(pandas_df.dtypes)
-    struct_list = []
-    i = 0
-    for column, typo in zip(columns, types): 
-      #print (column,typo)
-      struct_list.append(define_structure(column, typo))
-    p_schema = StructType(struct_list)
-    #print (p_schema)
-    return spark_session.createDataFrame(pandas_df, p_schema)
+    dtypeHeader = 'dtype'
+    actualHeader = 'actual'
+
+    def debug(self):
+        print(f"dataframe type{type(self.df)}")
+        print(f"dtypeHeader{self.dtypeHeader}")
+
+    def get_pdf_column_meta(self, column_name_set):
+        column_meta = {}
+        for ch in column_name_set:
+            column_meta.update({ch: {
+                self.dtypeHeader: str(self.df[ch].dtypes),
+                self.actualHeader: str(type(self.df[ch][0])).split("'")[1]
+            }})
+        return column_meta
+
+    def equivalent_type(self, dtype, actual):
+        if dtype == 'datetime64[ns]':
+            return DateType()
+        elif dtype == 'int64':
+            return LongType()
+        elif dtype == 'int32':
+            return IntegerType()
+        elif dtype == 'float64':
+            return FloatType()
+        elif dtype == "object" and actual == "list":
+            return ArrayType(StringType())
+        else:
+            return StringType()
+
+    def define_structure(self, column, tpe1, tpe2):
+        try:
+            typo = self.equivalent_type(tpe1, tpe2)
+        except:
+            typo = StringType()
+            print("not ok match type, resorting to string")
+        struct_field_return = StructField(column, typo)
+        return struct_field_return
+
+    def get_spark_df(self, df):
+        self.df = df
+        meta = self.get_pdf_column_meta(self.df.columns)
+        struct_list = []
+        for x in meta:
+            # tpe = col_attr(meta, str(x))
+            tpe = [str(meta.get(x).get(self.dtypeHeader)), str(meta.get(x).get(self.actualHeader))]
+            struct_list.append(self.define_structure(x, tpe[0], tpe[1]))
+        p_schema = StructType(struct_list)
+        return sqlContext.createDataFrame(self.df, p_schema)
+    
+ 
+# To use
+# load your pandas DF as pandasDF
+#  
+#from {path}.DataFrameConverter import DataFrameConverter as dfc
+#convertedToSpark = dfc().get_spark_df(pandasDF)
+#convertedToSpark.printSchema()
+#check your [ArrayType[StringType]] columns.
