5d711614541df679be843151e4bbb8de9aa5441a
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-04 22:41:16 +0530

    Update __init__.py

--- dsutils/__init__.py
+++ dsutils/__init__.py
@@ -1,5 +1,7 @@
 from dsutils_dev.dsutils.evaluate import get_eda_plots
 from dsutils_dev.dsutils.convert import DataFrameConverter
+from dsutils_dev.dsutils.colab_utils import mount_drive
+
 __version__ = '0.0.1'
 
 #from dsutils_dev.evaluate import get_eda_plots
