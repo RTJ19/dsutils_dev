6435ffe5976e8cdf2d2cebbff149657254c590ae
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-11 00:00:27 +0530

    Update __init__.py

--- dsutils/__init__.py
+++ dsutils/__init__.py
@@ -1,6 +1,6 @@
 from dsutils_dev.dsutils.evaluate import get_eda_plots
 from dsutils_dev.dsutils.convert import DataFrameConverter
-from dsutils_dev.dsutils.colab_utils import mount_drive #, get_spark_environment
+from dsutils_dev.dsutils.colab_utils import mount_drive, get_spark_environment
 
 __version__ = '0.0.1'
 
