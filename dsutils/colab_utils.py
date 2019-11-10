3de3f675f690ef0bba993d5943f652ebd5e81dd0
Roshan T John <38501352+RTJ19@users.noreply.github.com>
2019-11-10 18:22:41 +0530

    Update colab_utils.py
    Spark environment on Google colab

--- dsutils/colab_utils.py
+++ dsutils/colab_utils.py
@@ -5,3 +5,21 @@ def mount_drive(folder_name):
   drive.mount('/content/drive')
   file_path = "/content/drive/My Drive/"+ folder_name +"/"
   return file_path
+
+def get_spark_environment(extended=True):
+  !apt-get update
+  !apt-get install openjdk-8-jdk-headless -qq > /dev/null
+  !wget -q https://archive.apache.org/dist/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+  !tar xf spark-2.2.0-bin-hadoop2.7.tgz
+  !pip install -q findspark
+  !pip install pyspark
+  if extended == True:
+    !pip install pyspark_dist_explore
+    !pip install scikit-plot
+    
+  # setting the environment
+  import os
+  os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+  os.environ["SPARK_HOME"] = "/content/spark-2.2.0-bin-hadoop2.7"
+  import findspark
+  findspark.init()
